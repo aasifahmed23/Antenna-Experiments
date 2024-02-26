d7a7ae43d481f96d6d71067059d35eb4f7710afa
Asif Ahmed <raspberrynehuece@gmail.com>
2024-02-26 15:42:11 +0530

    Update quantized_amp.m

--- quantized_amp.m
+++ quantized_amp.m
@@ -1,27 +1,28 @@
-function [ quantResults ] = quantized_amp( vect, bits )
-%UNTITLED5 Summary of this function goes here
+function [ quantResults ] = quantized_phase( vect, bits )
+%UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 [M,N]= size(vect);
 if (M>1),
     vect=vect';
 end
-range = 1.0;
+range = 360;
 delta = roundVal(range/2*(bits));
 levels = 2^bits;
-base=0;
-quantValues=[0:delta:1]';
-inputArray=repmat(vect, lenght(quantValues), 1);
-quantArray=repmat(quantValues, 1, lenght(vect));
+base=-180;
+quantValues=[base:delta:180]';
+
+inputArray=repmat(vect, length(quantValues), 1);
+quantArray=repmat(quantValues, 1, length(vect));
+
 quantDiff=abs(inputArray-quantArray);
 [Y,I]= min(quantDiff);
 quantResults=quantArray(I);
 
+end
 function newVal = roundVal(num)
-     num = num*1e5;
+     num = num*1e4;
      num= round(num);
-     newVal= num*1e-5;
+     newVal= num*1e-4;
 
-
-
-
+end
 
